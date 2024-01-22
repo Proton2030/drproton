f46380d97f5502737e28914415f3860d2c50c8d5
tuhin <tuhin.thakur1233@gmail.com>
2024-01-23 00:57:40 +0530

    first_

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+solution that radiate positivity
           </h1>
         </motion.div>
 
@@ -33,12 +33,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+          Crafting Dreams,  
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Building Realities!{" "}
             </span>
-            project exprience
+           
           </span>
         </motion.div>
 
@@ -46,8 +46,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          Welcome&apos; to our project building haven, where innovation meets craftsmanship. With a passion for precision and an unwavering commitment to excellence, we transform visions into tangible realities. Each project is a symphony of creativity, functionality, and quality.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
@@ -64,8 +63,8 @@ const HeroContent = () => {
         <Image
           src="/mainIconsdark.svg"
           alt="work icons"
-          height={650}
-          width={650}
+          height={550}
+          width={550}
         />
       </motion.div>
     </motion.div>
